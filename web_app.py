9caaeb46a1aa0b1569f5e3acf54fc8a1ac6435ef
DoczyKristof <44243837+DoczyKristof@users.noreply.github.com>
2023-06-07 01:26:49 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -17,8 +17,8 @@ st.markdown(link, unsafe_allow_html=True)
 st.sidebar.header('Input Parameters')
 
 def user_input_features():
-    EU_Sales = st.sidebar.slider('EU_Sales', 0.01, 10.0, 29.02)
-    NA_Sales = st.sidebar.slider('NA_Sales', 0.01, 25.0, 41.49)
+    EU_Sales = st.sidebar.slider('EU_Sales', 0.01, 1.0, 29.02)
+    NA_Sales = st.sidebar.slider('NA_Sales', 0.01, 2.0, 41.49)
     JP_Sales = st.sidebar.slider('JP_Sales', 0.01, 4.0, 10.22)
     
     data = {'EU_Sales': EU_Sales,
