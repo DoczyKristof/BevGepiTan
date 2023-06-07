bdcb5ec46afc7e542cbaf26272b72a6fbf122b2e
DoczyKristof <44243837+DoczyKristof@users.noreply.github.com>
2023-06-07 11:24:56 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -45,7 +45,8 @@ st.subheader('Input parameters')
 st.write(df)
 
 # Create Plotly plot
-columns = ['EU_Sales (M dollars)', 'NA_Sales (M dollars)', 'JP_Sales (M dollars)']
+columns = ['EU_Sales', 'NA_Sales', 'JP_Sales']
+st.write("(numbers are in million dollars)")
 
 # create a new DataFrame with the selected columns
 df_game = df[columns]
